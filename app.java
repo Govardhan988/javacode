25c4856474a36d48a074e06086bb1c8e633cc72b
root <root@ip-172-31-41-79.ec2.internal>
2026-06-08 07:56:17 +0000

    java code

---
+++ app.java
@@ -0,0 +1,9 @@
+class Student {
+    public static void main(String[] args) {
+        String name = "Rahul";
+        int age = 20;
+
+        System.out.println("Student Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
